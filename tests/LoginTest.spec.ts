d46a6999b1f29e85a4f81a241d9a51d2c9a93be2
Carlos Gómez <cjga01@hotmail.com>
2026-06-22 14:07:49 -0400

    -Inclussion of all login tests

--- tests/LoginTest.spec.ts
+++ tests/LoginTest.spec.ts
@@ -4,28 +4,48 @@ import { LoginPage } from '../pages/LoginPage';
 
 test.describe('Login tests', () => {
 
-    // Paso previo que se ejecuta antes de cada test
     test.beforeEach(async ({ page }) => {
         await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     });
 
     test('Happy path - Login', async ({ page }) => {
-        // 1. Ingresar datos
         const loginpage = new LoginPage(page);
         await loginpage.navigateToLogin();
         await loginpage.checkLoginPageElements();
-        await loginpage.login('Admin','admin123');
-        // 3. Afirmar (Expect) el resultado esperado
+        const username = process.env.USER_NAME || '';
+        const password = process.env.USER_PASSWORD || '';
+        await loginpage.login(username,password);
         await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
     });
     test('Login w/ incorrect credentials', async ({ page }) => {
-        // 1. Ingresar datos
+        
         const loginpage = new LoginPage(page);
         await loginpage.checkLoginPageElements();
-        await loginpage.login('PepeAdmin','admin321')
-        
-        // 3. Afirmar (Expect) el resultado esperado
+        await loginpage.invalidFields();
         await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     });
-    
+    test('Login w/ empty Username field', async ({ page }) => {
+
+        const loginpage = new LoginPage(page);
+        await loginpage.checkLoginPageElements();
+        await loginpage.login('', 'admin321')
+        await expect(loginpage.usernameRequiredMessage).toBeVisible();
+        await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    });
+    test('Login w/ empty Password field', async ({ page }) => {
+
+        const loginpage = new LoginPage(page);
+        await loginpage.checkLoginPageElements();
+        await loginpage.login('PepeAdmin', '')
+        await expect(loginpage.passwordRequiredMessage).toBeVisible();
+        await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    });
+    test('Login w/ empty fields', async ({ page }) => {
+
+        const loginpage = new LoginPage(page);
+        await loginpage.checkLoginPageElements();
+        await loginpage.login('', '');
+        await loginpage.invalidFields();
+        await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    });
 });
